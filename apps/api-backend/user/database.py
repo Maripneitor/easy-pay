70c8927a4ed55f6db0b1eb0414d882445c3dbd2c
Erick Gama <erickgama797@gmail.com>
2026-03-19 17:00:42 -0600

    feat: Prevencion de uso redes publicas

--- apps/api-backend/user/database.py
+++ apps/api-backend/user/database.py
@@ -1,4 +1,3 @@
-# user/database.py
 import os
 import asyncio
 from urllib.parse import quote_plus
@@ -7,12 +6,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user = quote_plus(os.getenv("MONGO_USER", ""))
-password = quote_plus(os.getenv("MONGO_PASS", ""))
-cluster = os.getenv("MONGO_CLUSTER")
-db_name = os.getenv("DB_NAME_AUTH") 
 
-MONGO_URI = f"mongodb+srv://{user}:{password}@{cluster}/?retryWrites=true&w=majority"
+env_uri = os.getenv("MONGO_URI")
+
+if env_uri:
+    MONGO_URI = env_uri
+else:
+    user = quote_plus(os.getenv("MONGO_USER", ""))
+    password = quote_plus(os.getenv("MONGO_PASS", ""))
+    cluster = os.getenv("MONGO_CLUSTER")
+    MONGO_URI = f"mongodb+srv://{user}:{password}@{cluster}/?retryWrites=true&w=majority"
+
+
+db_name = os.getenv("DB_NAME_AUTH", "EasyPay_Auth")
 
 class MongoDB:
     def __init__(self):
@@ -20,16 +26,15 @@ class MongoDB:
         self.db = self.client[db_name]
 
     def get_collection(self, name: str):
-        # Esto usará la base de datos EasyPay_Auth que ya tienes
         return self.db[name]
 
     async def test_connection(self):
         try:
-            # El comando 'ping' verifica si Atlas nos responde
+            # El comando 'ping' verifica la conexión
             await self.client.admin.command('ping')
-            print(f"✅ Conexión exitosa a MongoDB Atlas: Base de datos '{db_name}' lista.")
+            print(f"Conexión exitosa ({'Local' if 'local' in MONGO_URI else 'Atlas'}): Base de datos '{db_name}' lista.")
         except Exception as e:
-            print(f"❌ Error conectando a MongoDB: {e}")
+            print(f"Error conectando a MongoDB: {e}")
 
 db_instance = MongoDB()
 
