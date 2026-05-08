d2d44e30ce2252649fd1de5b47fd633981848f74
Maripneitor <mariomoguel05@gmail.com>
2026-05-08 17:11:41 -0600

    nada de mobile y pdwa sirve

--- apps/mobile-app/babel.config.js
+++ apps/mobile-app/babel.config.js
@@ -1,21 +1,9 @@
 module.exports = function (api) {
-  api.cache.forever();
+  api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["."],
-          alias: {
-            "@": ".",
-          },
-        },
-      ],
-      "react-native-reanimated/plugin",
     ],
   };
 };
